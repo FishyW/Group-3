ad15c20daca9034872432c5209d343d8022191d2
JackRyderUSYD <jryd0588@uni.sydney.edu.au>
2026-04-14 15:20:10 +1000

    Create led.h

---
+++ code/Project-Tron/Inc/led.h
@@ -0,0 +1,34 @@
+#ifndef LED_H
+#define LED_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+// Logical LED numbering for the 8 Discovery board LEDs
+typedef enum {
+    LED0 = 0,
+    LED1,
+    LED2,
+    LED3,
+    LED4,
+    LED5,
+    LED6,
+    LED7
+} led_id_t;
+
+// Initialise all Discovery board LEDs
+void led_init(void);
+
+// Request/set one LED state
+void led_set(led_id_t led, bool state);
+
+// Read back the stored software state of one LED
+bool led_get(led_id_t led);
+
+// Read back the stored state of all LEDs as a bitmask
+uint8_t led_get_all(void);
+
+// Set all LEDs using a bitmask
+void led_set_all(uint8_t mask);
+
+#endif
