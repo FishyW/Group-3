afed3e83f0150ac204f69304bb2eb73b1250752c
Winston Wijaya <wwij0922@uni.sydney.edu.au>
2026-04-16 19:04:54 +1000

    finished double buffer advanced

--- code/Project-Tron/Src/uart.c
+++ code/Project-Tron/Src/uart.c
@@ -9,7 +9,6 @@
 #include "stm32f303xc.h"
 #include <string.h>
 
-#define DEFAULT_UART_METADATA {0,0,0,{},0,0,0}
 
 #define MAX_UART_BUFFER 256
 #define STX_CHARACTER 0x02
@@ -24,10 +23,13 @@ typedef struct ReceiverBufferMetadata {
 // note that switch buffer must be called by the kernel, since the kernel
 // can't be interrupted by the user
 typedef struct ReceiverDoubleBuffer {
+	// true when kernel has started writing data
+	uint8_t writeStarted;
+
 	// when the kernel has finished processing data writeFinished == 1
 	// kernel sets writeFinished to 0 or 1
 	uint8_t writeFinished;
-	// when the user has finished reading the data readReady == 1
+	// when the user has finished reading the data readFinished == 1
 	// kernel sets readFinished to 0, user sets readFinished to 1
 	uint8_t readFinished;
 
@@ -43,10 +45,7 @@ typedef struct UARTMetadata {
 	uint32_t sendBufferIndex;
 	uint32_t sendBufferSize;
 
-	uint8_t receiveBuffer[MAX_UART_BUFFER];
-	uint32_t receiveBufferSize;
-	uint8_t receiveInProgress;
-	uint8_t receiveError;
+	ReceiverDoubleBuffer doubleBuffer;
 } UARTMetadata;
 
 
@@ -67,7 +66,7 @@ struct _SerialPort {
 
 
 SerialPort USART1_PORT = {
-		DEFAULT_UART_METADATA,
+		{0},
 		USART1,
 		GPIOC,
 		RCC_APB2ENR_USART1EN, // bit to enable for APB2 bus
@@ -85,6 +84,7 @@ SerialPort USART1_PORT = {
 
 void sendNextByte(SerialPort *serial_port) {
 	UARTMetadata* metadata = &serial_port->metadata;
+
 	uint32_t index = metadata->sendBufferIndex;
 
 
@@ -115,8 +115,60 @@ void sendNextByte(SerialPort *serial_port) {
 
 }
 
+void switchBuffer(ReceiverDoubleBuffer* doubleBuffer) {
+	// switch the buffer when both the kernel and the user has finished reading
+	// and writing
+	if (doubleBuffer->readFinished && doubleBuffer->writeFinished) {
+		doubleBuffer->readFinished = 0;
+		doubleBuffer->writeStarted = 0;
+		doubleBuffer->writeFinished = 0;
+
+		doubleBuffer->kernelIndex = 1 - doubleBuffer->kernelIndex;
+	}
+}
+
+// ONLY KERNEL SHOULD CALL THIS
+// SIGNALS THE KERNEL IS NOW WRITING TO THE BUFFER
+// SWITCHES THE DOUBLE BUFFER IF USER HAS FINISHED READING THE BUFFER
+// IF THE USER STILL HASN'T READ IT IT OVERWRITES THE BUFFER
+void startBufferWrite(ReceiverDoubleBuffer* doubleBuffer) {
+	switchBuffer(doubleBuffer);
+
+	doubleBuffer->writeStarted = 1;
+	doubleBuffer->writeFinished = 0;
+
+
+}
+
+// GETS THE BUFFER
+ReceiverBufferMetadata* getBuffer(ReceiverDoubleBuffer* doubleBuffer, uint8_t isKernel) {
+	if (isKernel) {
+		return doubleBuffer->buffers + doubleBuffer->kernelIndex;
+	}
+	return doubleBuffer->buffers + (1 - doubleBuffer->kernelIndex);
+}
+
+// SIGNALS THAT THE USER HAS FINISHED READING THE BUFFER
+void finishBufferRead(ReceiverDoubleBuffer* doubleBuffer) {
+	doubleBuffer->readFinished = 1;
+}
+
+// SIGNALS THE KERNEL HAS FINISHED WRITING TO THE BUFFER
+// SWITCHES THE DOUBLE BUFFER IF USER HAS FINISHED READING THE BUFFER
+void finishBufferWrite(ReceiverDoubleBuffer* doubleBuffer) {
+	doubleBuffer->writeFinished = 1;
+
+	// switch the buffer when both the kernel and the user has finished reading
+	// and writing
+	switchBuffer(doubleBuffer);
+
+}
+
+
+
 void receiveNextByte(SerialPort *serial_port) {
-	UARTMetadata* metadata = &serial_port->metadata;
+	UARTMetadata* metadataAll = &serial_port->metadata;
+	ReceiverDoubleBuffer* doubleBuffer = &metadataAll->doubleBuffer;
 
 	// check that the receive register has something
 	// if not ready then return
@@ -124,32 +176,47 @@ void receiveNextByte(SerialPort *serial_port) {
 		return;
 	}
 
+	// read the buffer as the kernel
+	ReceiverBufferMetadata* metadata = getBuffer(doubleBuffer, 1);
+
 	// now read from the buffer, to prevent the interrupt from refiring
 	uint8_t ch =  (uint8_t) serial_port->UART->RDR;
 
-	// we need the second AND otherwise size of 0x02 or message id of 0x02
-	// or checksum == 0x02 leads to the buffer getting reset
-	if (ch == STX_CHARACTER && metadata->receiveInProgress == 0) {
-		metadata->receiveInProgress = 1;
+
+	// if the 1st charac is STX (i.e. buffer
+	if (ch == STX_CHARACTER && !doubleBuffer->writeStarted) {
+		// if buffer has been written, but there's a new incoming message
+		// set the buffer to not write finished, and replace the buffer with the new message
+		startBufferWrite(doubleBuffer);
+		metadata = getBuffer(doubleBuffer, 1);
+
 		metadata->receiveBufferSize = 0;
 		// clear the receive error
 		metadata->receiveError = 0;
 	}
 
-	// if STX bit not seen, then do nothing
-	if (metadata->receiveInProgress == 0) {
-		return;
+	// if STX bit is not seen, and the write hasn't started do nothing
+	if (!doubleBuffer->writeStarted) {
+		 return;
 	}
+
+	// if the write has finished try switching the buffer
+	// finishBufferWrite attempts to switch the buffer
+	if (doubleBuffer->writeFinished) {
+		return finishBufferWrite(doubleBuffer);
+	}
+
+
 
 
 	// if we reach max size of the buffer, we're done
 	// ERROR
 	if (metadata->receiveBufferSize >= MAX_UART_BUFFER) {
-		metadata->receiveInProgress = 0;
 		metadata->receiveBufferSize = 0;
 		metadata->receiveError = 1;
-		return;
+		return finishBufferWrite(doubleBuffer);
 	}
+
 
 	uint8_t claimedSize = 0;
 	if (metadata->receiveBufferSize >= 2) {
@@ -158,21 +225,28 @@ void receiveNextByte(SerialPort *serial_port) {
 
 
 	// if its bigger than the claimed size stop and ERROR
-	if (metadata->receiveBufferSize > claimedSize && claimedSize != 0) {
-		metadata->receiveInProgress = 0;
+	if (metadata->receiveBufferSize >= claimedSize && claimedSize != 0) {
 		metadata->receiveError = 1;
 		metadata->receiveBufferSize = 0;
-		return;
+		return finishBufferWrite(doubleBuffer);
 	}
 
-	if (metadata->receiveBufferSize == 12) {
+
+	if (metadata->receiveBufferSize >= 12) {
 		metadata->receiveError = 0;
 	}
 	// note that the second condition is necessary since 0x03 could appear
 	// in message id, the checksum or message length
 	if (metadata->receiveBuffer[metadata->receiveBufferSize - 1] == ETX_CHARACTER
 			&& metadata->receiveBufferSize + 1 == claimedSize) {
-		metadata->receiveInProgress = 0;
+
+		// copy the checksum over to the buffer
+		uint32_t size = metadata->receiveBufferSize;
+		metadata->receiveBuffer[size] = ch;
+		metadata->receiveBufferSize += 1;
+
+		// finish writing to the buffer
+		return finishBufferWrite(doubleBuffer);
 	}
 
 	// copy the character over to the buffer
@@ -274,6 +348,8 @@ void serialInitialise(SerialPort *serial_port, uint32_t baudRate, void (*complet
 
 	serial_port->completion_function = completion_function;
 
+	// set read finished to 1, since if read finished is 1, the user will spin
+	serial_port->metadata.doubleBuffer.readFinished = 1;
 }
 
 
@@ -314,80 +390,76 @@ uint8_t bcc_checksum(uint8_t* buffer, uint8_t size) {
 // Format: STX <message size> <message id> <buffer> ETX <checksum>
 // STX is 0x02, ETX is 0x03, the checksum used is BCC 8-bit XOR checksum
 void sendMsg(SerialPort *serial_port, uint8_t *buffer, uint8_t size, uint8_t message_id) {
-	uint8_t uart_buffer[MAX_UART_BUFFER];
-	uart_buffer[0] = STX_CHARACTER;
+	uint8_t uartBuffer[MAX_UART_BUFFER];
+	uartBuffer[0] = STX_CHARACTER;
 
 	// STX (1 byte), id (1 byte), size (1 byte), ETX (1 byte), checksum (1 byte)
 	// additional bytes = 5 bytes
-	uart_buffer[1] = size + 5;
-	uart_buffer[2] = message_id;
+	uartBuffer[1] = size + 5;
+	uartBuffer[2] = message_id;
 
 	for (int i = 3; i < size + 3; ++i) {
-		uart_buffer[i] = buffer[i-3];
+		uartBuffer[i] = buffer[i-3];
 	}
 
-	uart_buffer[size + 3] = ETX_CHARACTER;
-	uart_buffer[size + 4] = bcc_checksum(uart_buffer, size + 4);
+	uartBuffer[size + 3] = ETX_CHARACTER;
+	uartBuffer[size + 4] = bcc_checksum(uartBuffer, size + 4);
 
 
-	sendBuffer(serial_port, uart_buffer, size + 5);
+	sendBuffer(serial_port, uartBuffer, size + 5);
 
 
 }
 
 void receiveMsg(SerialPort *serial_port) {
-	UARTMetadata *metadata = &serial_port->metadata;
-	// if ISR is still receiving data or if nothing is in the buffer
-	// wait until something is in the buffer
-	while (1) {
-		// for now we'll put these to prevent race conditions
-		// ideally we should implement double buffers
-		__disable_irq();
-		if (!metadata->receiveInProgress && metadata->receiveBufferSize != 0) {
-			break;
-		}
-		__enable_irq();
-	};
+	UARTMetadata* metadataAll = &serial_port->metadata;
+	ReceiverDoubleBuffer* doubleBuffer = &metadataAll->doubleBuffer;
 
+	// if nothing is in the buffer, buffer is in state "read finished"
+	// once the kernel has switched the buffer the state transitions to read not finished
+	// in which case we read it
+	while (doubleBuffer->readFinished == 1) {};
+
+	// read the buffer as a user
+	ReceiverBufferMetadata* metadata = getBuffer(doubleBuffer, 0);
 
 	// parse the message
 	uint8_t* buffer = metadata->receiveBuffer;
 	uint8_t size = metadata->receiveBufferSize - 5;
-
-	// reset the buffer
-	metadata->receiveBufferSize = 0;
 
 	// VALIDATION CHECKS:
 	// set LED when validation checks fail
 
 	// check if receive error encountered
 	if (metadata->receiveError) {
-		metadata->receiveError = 0;
-		return;
+		return finishBufferRead(doubleBuffer);
 	}
 
 	// validation check: check if the buffer length is as claimed
 	if (size + 5 != buffer[1]) {
-		return;
+		return finishBufferRead(doubleBuffer);
 	}
 
 	// check the checksum is valid
 	if (bcc_checksum(buffer, size + 4) != buffer[size+4]) {
-		return;
+		return finishBufferRead(doubleBuffer);
 	}
 
 	// copy the buffer, since our interrupt might overwrite our old buffer
-	uint8_t new_buffer[MAX_UART_BUFFER];
+	uint8_t newBuffer[MAX_UART_BUFFER];
 	for (int i = 3; i < size + 3; ++i) {
-		new_buffer[i-3] = buffer[i];
+		newBuffer[i-3] = buffer[i];
 	}
 
 	uint8_t message_id = buffer[2];
-	__enable_irq();
+
+	finishBufferRead(doubleBuffer);
+
 	// call the completion function passed in
 	if (serial_port->completion_function != 0x00) {
-		serial_port->completion_function(new_buffer, size, message_id);
+		serial_port->completion_function(newBuffer, size, message_id);
 	}
+
 }
 
 
