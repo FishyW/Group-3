f1dc669285a2f9c0355a0539f1efaa8a597aefc6
Winston Wijaya <wwij0922@uni.sydney.edu.au>
2026-04-19 17:32:37 +1000

    integration A

--- code/Project-Tron/Inc/integration.h
+++ code/Project-Tron/Inc/integration.h
@@ -1,10 +1,19 @@
 #ifndef INTEGRATION_H
 #define INTEGRATION_H
 
+#include "integration.h"
+#include "sensors/magnetometer.h"
+
 #define DELAY_TIME_MILLISECONDS 100
+
+typedef struct {
+	MagSample magSample;
+	uint8_t displayState;
+} BoardMessage;
 
 void runBoardA();
 
 void runBoardB();
+
 
 #endif
--- code/Project-Tron/Src/integrationA.c
+++ code/Project-Tron/Src/integrationA.c
@@ -1,9 +1,17 @@
 // integration file for board 1
 #include "integration.h"
 
+#include "comm/i2c.h"
+#include "comm/uart.h"
+
+#include "timer.h"
+
 
 void initializeBoardA() {
-
+	i2c1Init();
+	serialInitialise(&USART1_PORT, BAUD_9600, 0x00);
+	initElapsedTimer();
+	magInit();
 }
 
 // Let there be 2 boards: Board A and Board B
@@ -17,16 +25,22 @@ void initializeBoardA() {
 // button state => 1, use LED array else use servo
 void runBoardA() {
 	initializeBoardA();
+	BoardMessage boardMsg;
 
 	while (1) {
+
 		// Jack
 		// magData = READ FROM I2C
+		 magReadSample(&boardMsg.magSample);
+
 		// read button toggle state
 
 		// Winston
 		// pass <magData + button state> to UART
+		 sendMsg(&USART1_PORT, (uint8_t*) &boardMsg, sizeof(BoardMessage), 0);
 
-		// Denny
-		// delay(DELAY_TIME_MILLISECONDS)
+		// set the delay to 10ms => 100Hz
+		 // which is the same as the magnetometer ODR
+		 delayElapsed(10);
 	}
 }
